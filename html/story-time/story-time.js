eba08a4c74ddb9456baececd7a3b11a018dd7840
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2021-06-17 20:16:44 +0800

    story time

--- html/story-time/story-time.js
+++ html/story-time/story-time.js
@@ -1,22 +1,8 @@
-var CorrectKey = "";
-
-
-var WordAudio = "";
-var nozoom = false;
-var key_string = "";
-var LessonString = "";
-var LessonStringPos = 0;
-var AllLetters = "ELKAİNOMUTÜYÖRIDSBZÇGŞCPHVĞFJ1234567890";
-
 var LessonLength = 11;
-var LessonProgress = 0;
+var LessonIndex = 0;
 
 var AllStoryData;
 var LessonStoryQuestionArray = [];
-var CorrectWordAudio = "";
-var CorrectWordString = "";
-
-var Timeout1, Timeout2, Timeout3, Timeout4, Timeout5, Timeout6, Timeout7, Timeout8, Timeout9, Timeout10;
 
 var LessonParameters;
 var LessonLanguage;
@@ -55,25 +41,33 @@ var _listener = function (playerid) {
 
     if (GotoNextQuestion) {
       GotoNextQuestion = false;
-      var ArrayID = LessonProgress - 1;
-      var CurrentStoryID = LessonStoryQuestionArray[ArrayID].story_id;
+      var CurrentStoryID = LessonStoryQuestionArray[LessonIndex].story_id;
 
       if (AnswerIsCorrect) {
         play_sound("../../audio/correct-sound/clap_2.mp3", "media_audio2", false);
 
         setTimeout(function () {
           AnswerIsCorrectLoadNext = true;
-          play_sound("../../audio/correct-sound/bravo-" + Math.floor((Math.random() * 10) + 1) + ".mp3", "media_audio");
+          play_sound("../../audio/correct-sound/bravo-" + Math.floor((Math.random() * 5) + 5) + ".mp3", "media_audio");
 
           $("#ballons").show();
           $("#ballons").addClass("balloons_hide");
 
-        }, 400);
+        }, 200);
 
       } else
       {
         play_sound("../../audio/wrong-sound/yanlis-15.mp3", "media_audio2", false);
-      }
+
+        if (!AddWordToEndOfList && LessonRedoWrongAnswers==="yes") {
+          LessonQuestionCount++;
+          AddWordToEndOfList = true;
+          var CurrentStoryID = LessonStoryQuestionArray[LessonIndex].story_id;
+          AllStoryData[CurrentStoryID].questions.push( AllStoryData[CurrentStoryID].questions[QuestionIndex] );
+        }
+
+
+        }
 
     }
 
@@ -82,16 +76,15 @@ var _listener = function (playerid) {
       $("#progress_bar_box").css({"width": ((LessonQuestionProgress / (LessonQuestionCount)) * 100) + "%"});
 
       AnswerIsCorrectLoadNext = false;
-      var ArrayID = LessonProgress - 1;
-      var CurrentStoryID = LessonStoryQuestionArray[ArrayID].story_id;
+      var CurrentStoryID = LessonStoryQuestionArray[LessonIndex].story_id;
       QuestionIndex++;
-      if (QuestionIndex < AllStoryData[CurrentStoryID].questions.length - 1) {
+      if (QuestionIndex < AllStoryData[CurrentStoryID].questions.length) {
         CreateQuestion();
       }
       else {
         QuestionIndex = 0;
-        LessonProgress++;
-        console.log(LessonProgress);
+        LessonIndex++;
+        console.log("Current Lesson Index: "+LessonIndex);
         CreateLesson();
       }
     }
@@ -99,54 +92,24 @@ var _listener = function (playerid) {
 };
 
 
-function CreateLesson() {
-  var ArrayID = LessonProgress - 1;
-  var CurrentStoryID = LessonStoryQuestionArray[ArrayID].story_id;
-  AddWordToEndOfList = false;
-
-  play_sound("", "media_audio2", true);
-
-  clearTimeout(Timeout1);
-  clearTimeout(Timeout2);
-  clearTimeout(Timeout3);
-  clearTimeout(Timeout4);
-  clearTimeout(Timeout5);
-  clearTimeout(Timeout6);
-  clearTimeout(Timeout7);
-  clearTimeout(Timeout8);
-  clearTimeout(Timeout9);
-  clearTimeout(Timeout10);
-
-  console.log(ArrayID);
-  $("#ballons").hide();
-  $("#story_div").show();
-  $("#question_div").hide();
-  $("#story_picture").attr('src', 'poster://pictures/story/' + AllStoryData[CurrentStoryID].picture);
-
-  $("#story_title").html(AllStoryData[CurrentStoryID].title);
-  $("#story_text").html(AllStoryData[CurrentStoryID].story.replace(/\n/gi, "<br>"));
-
-  play_sound("poster://audio/story/" + AllStoryData[CurrentStoryID].audio, "media_audio", false);
-
-  console.log(AllStoryData[CurrentStoryID]);
-  $("#start_questions").hide();
-  LessonIntroductionPlaying = true;
-  QuestionIndex = 0;
-  AllStoryData[CurrentStoryID].questions = shuffleArray(AllStoryData[CurrentStoryID].questions);
-}
-
 function CreateQuestion() {
-  var ArrayID = LessonProgress - 1;
-  var CurrentStoryID = LessonStoryQuestionArray[ArrayID].story_id;
+  var CurrentStoryID = LessonStoryQuestionArray[LessonIndex].story_id;
   var ThisQuestion = AllStoryData[CurrentStoryID].questions[QuestionIndex];
 
+  console.log("question index: "+QuestionIndex+", Lesson Index: "+LessonIndex+" CurrentStoryID: "+CurrentStoryID+" total questions: "+AllStoryData[CurrentStoryID].questions.length);
+
   AnswerIsCorrect = false;
+  AddWordToEndOfList = false;
 
   $("#ballons").hide();
   play_sound("", "media_audio2", true);
 
   $("#question_div").show();
-  $("#question_picture").attr('src', 'poster://pictures/story-question/' + ThisQuestion.picture);
+  if (ThisQuestion.picture==="" || ThisQuestion.picture===null) {
+    $("#question_picture").attr('src', 'question_mark_icon.png');
+  } else {
+    $("#question_picture").attr('src', 'poster://pictures/story-question/' + ThisQuestion.picture);
+  }
 
   $("#question_title").html(ThisQuestion.question);
 //  $("#question_answers").html(  AllStoryData[ CurrentStoryID ].story.replace(/\n/gi,"<br>"));
@@ -198,6 +161,31 @@ function CreateQuestion() {
 }
 
 
+function CreateLesson() {
+  var CurrentStoryID = LessonStoryQuestionArray[LessonIndex].story_id;
+  AddWordToEndOfList = false;
+
+  play_sound("", "media_audio2", true);
+
+  console.log("CreateLesson Lesson Index: "+LessonIndex);
+  $("#ballons").hide();
+  $("#story_div").show();
+  $("#question_div").hide();
+  $("#story_picture").attr('src', 'poster://pictures/story/' + AllStoryData[CurrentStoryID].picture);
+
+  $("#story_title").html(AllStoryData[CurrentStoryID].title);
+  $("#story_text").html(AllStoryData[CurrentStoryID].story.replace(/\n/gi, "<br>"));
+
+  play_sound("poster://audio/story/" + AllStoryData[CurrentStoryID].audio, "media_audio", false);
+
+  console.log(AllStoryData[CurrentStoryID]);
+  $("#start_questions").hide();
+  LessonIntroductionPlaying = true;
+  QuestionIndex = 0;
+  AllStoryData[CurrentStoryID].questions = shuffleArray(AllStoryData[CurrentStoryID].questions);
+}
+
+
 $(document).ready(function () {
 
   LessonParameters = window.sendSyncCmd('get-lesson-parameters', '');
@@ -236,6 +224,8 @@ $(document).ready(function () {
     $("#story_grid").append("<div data-id='" + i + "' class='story_select'><img src='poster://pictures/story/" + AllStoryData[i].picture + "'  class='story_select_image'><div style='background-color: #6666aa; color:white; text-align: center; padding:5px; border-radius: 4px;'>" + AllStoryData[i].title + "</div></div>");
   }
 
+  $("#story_grid").shuffleChildren();
+
   $(".story_select").on('click', function () {
     console.log($(this).data("id"));
     $(this).toggleClass("story_selected");
@@ -247,8 +237,7 @@ $(document).ready(function () {
 
   $("#start_questions").on('click', function () {
     $("#start_questions").hide();
-    var ArrayID = LessonProgress - 1;
-    if (AllStoryData[LessonStoryQuestionArray[ArrayID].story_id].hide_after_intro === "1") {
+    if (AllStoryData[LessonStoryQuestionArray[LessonIndex].story_id].hide_after_intro === "1") {
       $("#story_div").hide();
     }
 
@@ -266,16 +255,15 @@ $(document).ready(function () {
 
       LessonStoryQuestionArray.push({"story_id": story_id, "done": false});
 
-      for (var i = 0; i < AllStoryData[story_id].questions.length; i++) {
-        LessonQuestionCount++;
+      LessonQuestionCount += AllStoryData[story_id].questions.length;
+      // for (var i = 0; i < AllStoryData[story_id].questions.length; i++) {
 //         LessonStoryQuestionArray.push({"story_id": story_id, "question_id": i, "done": false});
-      }
-      console.log(LessonStoryQuestionArray);
+//       }
     });
+    console.log(LessonStoryQuestionArray);
 
     LessonLength = LessonStoryQuestionArray.length;
-    LessonProgress++;
-    console.log(LessonProgress);
+    LessonIndex = 0;
     CreateLesson();
   });
 
